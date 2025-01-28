538fb99cda87dd05e0d4e41dc19c34aaa2c93737
DeveloperIRkhan <programmer.irfansha@hotmail.com>
2025-01-28 14:54:04 +0500

    updating and deleting category

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -56,3 +56,80 @@ export const getAllCategoryController = async (req, resp) => {
     console.log(error);
   }
 };
+export const RemoveCategoryByIdController = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const category = await Category.findByIdAndDelete(id);
+    if (!category) {
+      return res.status(200).json({
+        success: true,
+        message: "category not found"
+      });
+    }
+    return res.status(200).json({ success: true, message: "category removed" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "something went wrong."
+    });
+  }
+};
+
+export const getCategoryById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const existingCategory = await Category.findById(id);
+    if (!existingCategory) {
+      return res.status(404).json({ message: "Category not found" });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "category found",
+      category: existingCategory
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: `something went wrong, ${error}`
+    });
+  }
+};
+export const UpdateCategoryByIdController = async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log("Id", id);
+    const { updateCategory } = req.body;
+    console.log("category", updateCategory);
+    const existingCategory = await Category.findById(id);
+    if (!existingCategory) {
+      return res.status(404).json({ message: "Category not found" });
+    }
+    if ([updateCategory].some((field) => field?.trim() === "")) {
+      return res.status(400).json({ message: "Please fill all fields" });
+    }
+    const updatedCategory = await Category.findByIdAndUpdate(
+      id,
+      {
+        $set: { category: updateCategory }
+      },
+      {
+        new: true
+      }
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "category Updated successfully",
+      category: updatedCategory
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: `category not updated, ${error}`
+    });
+  }
+};
--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,8 +1,23 @@
 import { Router } from "express";
-import { createCategoryController, getAllCategoryController } from "../controllers/category.controller.js";
+import {
+  createCategoryController,
+  getAllCategoryController,
+  getCategoryById,
+  RemoveCategoryByIdController,
+  UpdateCategoryByIdController
+} from "../controllers/category.controller.js";
+import { IsAdmin } from "../middleware/IsAdmin.middleware.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const categoryRoute = new Router();
-categoryRoute.route("/create-category").post(createCategoryController)
-categoryRoute.route("/get-categorys").get(getAllCategoryController)
+categoryRoute.route("/create-category").post(createCategoryController);
+categoryRoute.route("/get-categorys").get(getAllCategoryController);
+categoryRoute.route("/get-category/:id").get(getCategoryById);
+categoryRoute
+  .route("/remove-category/:id")
+  .delete(verifyJWT, IsAdmin, RemoveCategoryByIdController);
+categoryRoute
+  .route("/update-category/:id")
+  .post(verifyJWT, IsAdmin, UpdateCategoryByIdController);
 
 export default categoryRoute;
